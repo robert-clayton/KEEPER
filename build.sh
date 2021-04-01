75f104f60507a66e8c14d4ad7462e9072d26454d
robert-clayton <41345902+robert-clayton@users.noreply.github.com>
2021-03-31 23:27:33 -0700

    Cmake!

--- build.sh
+++ build.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-$SCRIPTROOT="$(dirname $0)"
-$OUTPUTPATH="$SCRIPTROOT/dist/"
-$OUTPUTNAME="KEEPER"
-$SDLINCLUDEPATH="/usr/include/" # or whatever
-$SDLLIBPATH="/usr/lib" # or whatever
+# Make sure we have a 'build' folder.
+if [ ! -d "build" ]; then
+    mkdir build
+fi
 
-$SOURCEPRIVATE=("/wherever"/*.cpp "/wherever/ecs/"*.cpp)
-$SOURCEPUBLIC=("$SCRIPTROOT/src" "$SCRIPTROOT/src/ecs")
+# Remove the 'out' folder if it exists.
+if [ -d "out" ]; then
+    rm -rf out
+fi
 
-# Ensure output path exists
-mkdir -p $OUTPUTPATH
+# In order to find the Emscripten build tools, we need to configure some environment variables so they are available during the build. The required environment variables are initialized by sourcing the 'emsdk_env.sh' that ships with the Emscripten SDK.
+pushd .emsdk
+    echo "Configuring Emscripten environment variables"
+    . ./emsdk_env.sh
+popd
 
-# Construct g++ command
-$COMMAND="g++ -g "
-for i in "${SOURCEPRIVATE[@]}" # bash arrays are ass
-do
-  $COMMAND="$COMMAND'$i' " # sprinkling single quotes into $COMMAND around paths in case they have spaces
-done
-$COMMAND="$COMMAND-o '$OUTPUTPATH$OUTPUTNAME' "
-for i in "${SOURCEPUBLIC[@]}"
-do
-  $COMMAND="$COMMAND-I'$i' "
-done
-$COMMAND="$COMMAND-I'$SDLINCLUDEPATH' -L'$SDLLIBPATH' -lmingw32 -lSDL2main -lSDL2 -lSDL2_Image -mwindows"
+# Hop into the 'build' folder to start our CMake build.
+pushd build
+	
+    # Because we sourced the Emscripten environment variables, we can use the 'EMSCRIPTEN' var to know where the current SDK can be found, which we need so we can locate the 'Emscripten.cmake' toolchain file.
+    EMSCRIPTEN_CMAKE_PATH=${EMSDK}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 
-echo $COMMAND
-eval $COMMAND
+    # We ask CMake to configure itself against the parent folder, but unlike our other platform targets, we will tell CMake to use the Emscripten CMake toolchain which knows how to perform Emscripten builds.
+    echo "Emscripten CMake path: ${EMSCRIPTEN_CMAKE_PATH}"
+    cmake -DCMAKE_TOOLCHAIN_FILE=${EMSCRIPTEN_CMAKE_PATH} ..
+
+    # Start the actual build.
+    echo "Building project ..."
+    make
+popd
